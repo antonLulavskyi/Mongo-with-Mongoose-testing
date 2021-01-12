56a9bf7c1114023170ad35a74cbc0c6a6596293f
Marcin <a@as-MacBook-Pro.local>
2021-01-12 14:07:09 +0100

    Establishing a relationships between the collections.

--- app.js
+++ app.js
@@ -31,18 +31,48 @@ const fruit = new Fruit({
   review: "Pretty solid as a fruit"
 });
 
+// Create a new schema/model for people collection
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favoritFruit: fruitSchema
+});
+
+// Create a people collection
+const Person = mongoose.model("Person", personSchema);
+
+
+
 const kivi = new Fruit({
   name: "Kivi",
   rating: 8,
   review: "Nice"
 });
 
-const banana = new Fruit({
-  name: "banana",
-  rating: 11,
-  review: "The best fruit ever"
+const pineapple = new Fruit({
+  name: "Pineapple",
+  rating: 9,
+  review: "Nice fruit"
 })
 
+ // kivi.save();
+
+const person = new Person({
+  name: "Anna",
+  age: 16,
+  favoritFruit: pineapple
+});
+
+// Person.updateOne({_id: "5ffd9ad385b9c40f9dda589f"}, {favoritFruit: kivi}, function(err) {
+//   if(err) {
+//     console.log(err)
+//   } else {
+//     mongoose.connection.close();
+//     console.log("James has been updated.");
+//   }
+// });
+
+// person.save();
 // Fruit.insertMany([kivi, banana], function(err) {
 //   if(err) {
 //     console.log(err);
@@ -54,33 +84,21 @@ const banana = new Fruit({
 // fruit.save();
 // banana.save();
 
-// Create a new schema/model for people collection
-const personSchema = new mongoose.Schema({
-  name: String,
-  age: Number
-});
 
-// Create a people collection
-const Person = mongoose.model("Person", personSchema);
-
-const person = new Person({
-  name: "James",
-  age: 25
-});
 
 // person.save();
 
-Fruit.find(function(err, fruits) {
-  if (err) {
-    console.log(err);
-  } else {
-    mongoose.connection.close();
-    fruits.forEach(function(fruit) {
-      fruitsName.push(fruit.name);
-    });
-    console.log(fruitsName);
-  }
-})
+// Fruit.find(function(err, fruits) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     mongoose.connection.close();
+//     fruits.forEach(function(fruit) {
+//       fruitsName.push(fruit.name);
+//     });
+//     console.log(fruitsName);
+//   }
+// })
 
 // Fruit.updateOne({ _id: "5ffd79e47ad8030ca94e4707"},{ rating: 1},function(err) {
 //     if(err) {
@@ -98,10 +116,10 @@ Fruit.find(function(err, fruits) {
   //   }
   // });
 
-  Fruit.deleteMany( { name: "banana"}, function(err) {
-    if(err) {
-      console.log(err);
-    } else {
-      console.log("Successfully deleted items.");
-    }
-  });
+  // Person.deleteMany( { name: "James"}, function(err) {
+  //   if(err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log("Successfully deleted items.");
+  //   }
+  // });
