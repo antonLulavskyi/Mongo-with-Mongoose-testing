09d16f285f7f7e491aaa4711afe04d64732e1c04
Marcin <a@as-MacBook-Pro.local>
2021-01-12 11:51:27 +0100

    Pulling out data from MangoDB and placing into local array.

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
+
+const fruitsName = [];
 // Loking for connection on port 27017 and db fruitsDB, if it's doesn't exist mongoose will create fruitsDB
-mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 // Create a new schema/model for fruits collection
-const fruitSchema = new mongoose.Schema ({
+const fruitSchema = new mongoose.Schema({
   name: String,
   rating: Number,
   review: String
@@ -13,7 +18,7 @@ const fruitSchema = new mongoose.Schema ({
 // Create a collection
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
-const fruit = new Fruit ({
+const fruit = new Fruit({
   name: "Apple",
   rating: 10,
   review: "Pretty solid as a fruit"
@@ -25,7 +30,7 @@ const kivi = new Fruit({
   review: "Nice"
 });
 
-const banana = new Fruit ({
+const banana = new Fruit({
   name: "banana",
   score: 10,
   review: "The best fruit ever"
@@ -41,7 +46,7 @@ const banana = new Fruit ({
 // fruit.save();
 
 // Create a new schema/model for people collection
-const personSchema = new mongoose.Schema ({
+const personSchema = new mongoose.Schema({
   name: String,
   age: Number
 });
@@ -49,13 +54,20 @@ const personSchema = new mongoose.Schema ({
 // Create a people collection
 const Person = mongoose.model("Person", personSchema);
 
-const person = new Person ({
+const person = new Person({
   name: "James",
   age: 25
 });
 
 // person.save();
 
-fruit.find(function(err, fruit) {
-  
+Fruit.find(function(err, fruits) {
+  if (err) {
+    console.log(err);
+  } else {
+    fruits.forEach(function(fruit) {
+      fruitsName.push(fruit.name);
+    });
+    console.log(fruitsName);
+  }
 })
